df69b30c25f2c5af3f44c500d896aabdb0929a76
SHUBHAM WAGHMARE <SHUBHAM WAGHMARE@DESKTOP-9E0ADTQ>
2023-03-08 14:07:56 +0530

    Arithmetic_operations_assignments_combined

---
+++ src/a_testing_Assignment/Arithmetic_operations.java
@@ -0,0 +1,72 @@
+package a_testing_Assignment;
+
+public class Arithmetic_operations {
+public static void main(String[] args) {
+//	int i = 10;
+//
+//	int j = 7;
+//
+//	System.out.println(i + j);
+//
+//	System.out.println(i - j);
+//
+//	System.out.println(i * j);
+//
+//	System.out.println(i / j);
+//
+//	System.out.println(i % j);
+// outputs
+//	17
+//	3
+//	70
+//	1
+//	3
+//-------------------------------------
+//	int m = 10;
+//
+//	int n= 7; 
+
+//	double db1 = m / n;
+//	System.out.println(db1); // o/p 1.0
+//	double db1 = (double) m / n;
+//	System.out.println(db1); // o/p 1.4285714285714286
+//	double db1 = m / (double) n;
+//	System.out.println(db1); // o/p  1.4285714285714286
+//-------------------------------------------------------------------------
+
+	// int i = 10;
+//
+//	int j = i++; 
+//
+//	System.out.println(i); // i = 11
+//
+//	System.out.println(j); // j = 10
+
+//	int i = 10;
+//
+//	int j = ++i; 
+//
+//	System.out.println(i); // i =  11 
+//
+//	System.out.println(j); // j =  11
+//
+	
+//	int i = 10; 
+//
+//	int j = i++ + ++i; 
+//
+//	System.out.println(i); // i= 12
+//
+//	System.out.println(j); // j = 22
+
+	
+//	int i = 10;
+//
+//	int j = i++ + i++ + --i + ++i + ++i; 
+//           10 + 11 + 11 + 12+ 13
+//	System.out.println(i); // i = 13
+//
+//	System.out.println(j);// j= 57
+
+}
+}
