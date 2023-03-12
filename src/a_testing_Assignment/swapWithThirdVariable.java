253d41dfcb850be1fe56c69a2c78795cc5b98748
SHUBHAM WAGHMARE <SHUBHAM WAGHMARE@DESKTOP-9E0ADTQ>
2023-03-12 21:16:25 +0530

    code here

---
+++ src/a_testing_Assignment/swapWithThirdVariable.java
@@ -0,0 +1,16 @@
+package a_testing_Assignment;
+
+public class swapWithThirdVariable {
+public static void main(String[] args) {
+	int i = 10;
+	int j = 7;
+	int k = i;
+	 i = j;
+	 j = k;
+
+    System.out.println("new value of i is : " + i);
+
+    System.out.println("new value of j is : " + j);
+
+}
+}
