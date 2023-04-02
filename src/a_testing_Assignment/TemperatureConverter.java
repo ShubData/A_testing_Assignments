bec2b900b4d45cf2dd5ce68a0a1345056fa83839
SHUBHAM WAGHMARE <SHUBHAM WAGHMARE@DESKTOP-9E0ADTQ>
2023-04-02 12:41:43 +0530

    this is assignment reuploaded

--- src/a_testing_Assignment/TemperatureConverter.java
+++ src/a_testing_Assignment/TemperatureConverter.java
@@ -1,5 +1,6 @@
 package a_testing_Assignment;
 import java.util.Arrays;
+import java.util.Scanner;
 public class TemperatureConverter {
 static double[] gettempconverted(double c) {
 	double f = 1.8*c + 32;
@@ -13,8 +14,12 @@ static double[] gettempconverted(double c) {
 }
 
 public static void main(String[] args) {
-	
-double b[] =TemperatureConverter.gettempconverted(0);
+
+Scanner sc = new Scanner(System.in);
+System.out.println("Please Enter celsius value to convert Below:");
+double a = sc.nextDouble();
+double b[] =TemperatureConverter.gettempconverted(a);
 System.out.println(Arrays.toString(b));
+sc=null;
 }
 }
